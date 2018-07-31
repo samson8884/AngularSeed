f527ddc313d7d77c2de799fbcb04547175d2c847
Xavier Pinto <xavpin71@in.ibm.com>
2018-07-31 14:04:34 +0530

    - added theme - added content on home page and about page - added animate.css

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../authentication/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.username = this.authService.getToken().split("@")[0];
   }
 
 }
